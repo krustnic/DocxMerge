3a0a4c558704074bf34e405171e586d087f0ae2d
zlob <vamakin@gmail.com>
2015-11-02 16:33:12 +0300

    remove .idea folder

--- tests/DocxMergeTest.php
+++ tests/DocxMergeTest.php
@@ -10,7 +10,4 @@
             $doc = new DocxMerge();
             $doc->setValues(__DIR__.'\test_src\test_doc.docx', __DIR__.'\test_result\test_'.time().'.docx', ['name'=>'test_name']);
         }
-
-
-
     }
