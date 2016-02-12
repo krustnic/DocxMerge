9e4c9818e65ed6a8242b320dbd88fe3adb502cc3
Nuno <nunochaves@sapo.pt>
2016-02-12 15:42:34 +0000

    added composer

---
+++ src/Docx.php
@@ -0,0 +1,134 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: krustnic
+ * Date: 05.02.14
+ * Time: 11:59
+ */
+namespace Jupitern\DocxMerge;
+
+
+class Docx {
+
+    // Path to current docx file
+    private $docxPath;
+
+    // Current _RELS data
+    private $docxRels;
+    // Current DOCUMENT data
+    private $docxDocument;
+    // Current CONTENT_TYPES data
+    private $docxContentTypes;
+
+    private $docxZip;
+
+    private $RELS_ZIP_PATH          = "word/_rels/document.xml.rels";
+    private $DOC_ZIP_PATH           = "word/document.xml";
+    private $CONTENT_TYPES_PATH     = "[Content_Types].xml";
+    private $ALT_CHUNK_TYPE         = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/aFChunk";
+    private $ALT_CHUNK_CONTENT_TYPE = "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml";
+
+    // Array "zip path" => "content"
+    private $headerAndFootersArray = [];
+
+    public function __construct( $docxPath ) {
+        $this->docxPath = $docxPath;
+
+        $this->docxZip = new TbsZip();
+        $this->docxZip->Open( $this->docxPath );
+
+        $this->docxRels = $this->readContent( $this->RELS_ZIP_PATH );
+        $this->docxDocument = $this->readContent( $this->DOC_ZIP_PATH );
+        $this->docxContentTypes = $this->readContent( $this->CONTENT_TYPES_PATH );
+    }
+
+    private function readContent( $zipPath ) {
+        $content = $this->docxZip->FileRead( $zipPath );
+
+        return $content;
+    }
+
+    private function writeContent( $content, $zipPath ) {
+        $this->docxZip->FileReplace($zipPath, $content, TBSZIP_STRING);
+        return 0;
+    }
+
+    public function addFile( $filePath, $zipName, $refID, $addPageBreak = false ) {
+        $content = file_get_contents( $filePath );
+        $this->docxZip->FileAdd( $zipName, $content );
+
+        $this->addReference( $zipName, $refID );
+        $this->addAltChunk( $refID, $addPageBreak );
+        $this->addContentType( $zipName );
+    }
+
+    private function addReference( $zipName, $refID ) {
+        $relXmlString = '<Relationship Target="../'.$zipName.'" Type="'.$this->ALT_CHUNK_TYPE.'" Id="'.$refID.'"/>';
+
+        $p = strpos($this->docxRels, '</Relationships>');
+        $this->docxRels = substr_replace($this->docxRels, $relXmlString, $p, 0);
+    }
+
+    private function addAltChunk( $refID, $addPageBreak ) {
+        $pagebreak = $addPageBreak ? '<w:p><w:r><w:br w:type="page" /></w:r></w:p>' : '';
+        $xmlItem = $pagebreak.'<w:altChunk r:id="'.$refID.'"/>';
+
+        $p = strpos($this->docxDocument, '</w:body>');
+        $this->docxDocument = substr_replace($this->docxDocument, $xmlItem, $p, 0);
+    }
+
+    private function addContentType( $zipName ) {
+        $xmlItem = '<Override ContentType="'.$this->ALT_CHUNK_CONTENT_TYPE.'" PartName="/'.$zipName.'"/>';
+
+        $p = strpos($this->docxContentTypes, '</Types>');
+        $this->docxContentTypes = substr_replace($this->docxContentTypes, $xmlItem, $p, 0);
+    }
+
+    public function loadHeadersAndFooters() {
+        $relsXML = new SimpleXMLElement( $this->docxRels );
+        foreach( $relsXML as $rel ) {
+            if ( $rel["Type"] == "http://schemas.openxmlformats.org/officeDocument/2006/relationships/footer" ||
+                 $rel["Type"] == "http://schemas.openxmlformats.org/officeDocument/2006/relationships/header" ) {
+                $path = "word/".$rel["Target"];
+                $this->headerAndFootersArray[ $path ] = $this->readContent( $path );
+            }
+        }
+    }
+
+    public function findAndReplace( $key, $value ) {
+        // Search/Replace in document
+        $this->docxDocument = str_replace( $key, $value, $this->docxDocument );
+        // Search/Replace in footers and headers
+        foreach( $this->headerAndFootersArray as $path => $content ) {
+            $this->headerAndFootersArray[$path] = str_replace( $key, $value, $content );
+        }
+    }
+
+    public function flush() {
+        // Save RELS data
+        $this->writeContent( $this->docxRels, $this->RELS_ZIP_PATH );
+        // Save DOCUMENT data
+        $this->writeContent( $this->docxDocument, $this->DOC_ZIP_PATH );
+        // Save CONTENT TYPES data
+        $this->writeContent( $this->docxContentTypes, $this->CONTENT_TYPES_PATH );
+        // Save footers and headers
+        foreach( $this->headerAndFootersArray as $path => $content ) {
+            $this->writeContent( $content, $path );
+        }
+
+        // Save the merge into a third file
+        // We cannot save to current file because it damage ZIP file
+        $tempFile = tempnam( dirname( $this->docxPath ), "dm" );
+
+        $this->docxZip->Flush(TBSZIP_FILE, $tempFile);
+
+        // Replace current file with tempFile content
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            copy( $tempFile, $this->docxPath );
+        }
+        else {
+            rename($tempFile, $this->docxPath);
+        }
+    }
+
+} 
---
+++ src/DocxMerge.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * User: krustnic
+ * Date: 04.02.14
+ * Time: 11:17
+ */
+
+namespace Jupitern\DocxMerge;
+
+class DocxMerge
+{
+
+	/**
+	 * Merge files in $docxFilesArray order and
+	 * create new file $outDocxFilePath
+	 * @param $docxFilesArray
+	 * @param $outDocxFilePath
+	 * @return int
+	 */
+	public function merge($docxFilesArray, $outDocxFilePath, $addPageBreak = false)
+	{
+		if (count($docxFilesArray) == 0) {
+			// No files to merge
+			return -1;
+		}
+
+		if (substr($outDocxFilePath, -5) != ".docx") {
+			$outDocxFilePath .= ".docx";
+		}
+
+		if (!copy($docxFilesArray[0], $outDocxFilePath)) {
+			// Cannot create file
+			return -2;
+		}
+
+		$docx = new Docx($outDocxFilePath);
+		for ($i = 1; $i < count($docxFilesArray); $i++) {
+			$docx->addFile($docxFilesArray[$i], "part" . $i . ".docx", "rId10" . $i, $addPageBreak);
+		}
+
+		$docx->flush();
+
+		return 0;
+	}
+
+	public function setValues($templateFilePath, $outputFilePath, $data)
+	{
+
+		if (!file_exists($templateFilePath)) {
+			return -1;
+		}
+
+		if (!copy($templateFilePath, $outputFilePath)) {
+			// Cannot create output file
+			return -2;
+		}
+
+		$docx = new Docx($outputFilePath);
+		$docx->loadHeadersAndFooters();
+		foreach ($data as $key => $value) {
+			$docx->findAndReplace("\${" . $key . "}", $value);
+		}
+
+		$docx->flush();
+	}
+
+}
